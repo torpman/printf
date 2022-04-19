cc6e63ef17f82248d2b37beeb1ca599e71e8732d
torpman <torpman1112@gmail.com>
2022-04-19 04:12:25 -0700

    Hopefully fixed

---
+++ _strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _strlen - The function find the lenght of any string passed
+*
+* @str: The string to get its lenght
+*
+* Return: Return the string lenght
+*/
+
+int _strlen(char *str)
+{
+	int len;
+
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+
+	return (len);
+}
