bdb0c629042275118af18ba420765e6cacdf4e64
torpman <torpman1112@gmail.com>
2022-04-18 20:26:20 -0700

    Print_char debugged

--- print_char.c
+++ print_char.c
@@ -8,14 +8,12 @@
  */
 int print_char(va_list *c)
 {
-	char ch = va_arg(*c, int);
-
-	int j = sizeof(ch);
+	char ch;
+	int j;
+	
+	ch = va_arg(*c, int);
+	j = sizeof(ch);
 	write(1, &ch, j);
-
-
-	int j = sizeof(ch)
-		write(1, &ch, j);
 
 	return (j);
 }
