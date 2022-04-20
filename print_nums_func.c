1501cdfac1fdf5bf57f0065ff586a96e3722c3c5
torpman <torpman1112@gmail.com>
2022-04-19 17:32:57 -0700

    numbers specifiers updated

--- print_nums_func.c
+++ print_nums_func.c
@@ -38,3 +38,35 @@ int print_int(va_list i)
 	}
 	return (count);
 }
+
+/**
+ * print_uint - prints an unsigned in in decimal notation
+ * @u: unsigned int to print
+ *
+ * Return: number of digits printed
+ */
+int print_uint(va_list u)
+{
+	unsigned int a[10];
+	unsigned int i, m, n, sum;
+	int count;
+
+	n = va_arg(u, unsigned int);
+	m = 1000000000; /* (10 ^ 9) */
+	a[0] = n / m;
+	for (i = 1; i < 10; i++)
+	{
+		m /= 10;
+		a[i] = (n / m) % 10;
+	}
+	for (i = 0, sum = 0, count = 0; i < 10; i++)
+	{
+		sum += a[i];
+		if (sum || i == 9)
+		{
+			_print('0' + a[i]);
+			count++;
+		}
+	}
+	return (count);
+}
