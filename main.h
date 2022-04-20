c74f11360813a63002a7d6161c1398e489d12e80
Ninikay <ms.nikeoni@gmail.com>
2022-04-20 01:29:45 +0000

    updated prototype

--- main.h
+++ main.h
@@ -22,8 +22,7 @@ int print_uint(va_list u);
 int print_oct(va_list o);
 int print_ex(va_list x);
 int print_HEX(va_list X);
-unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
-int print_buf(char *buf, unsigned int nbuf);
 int print_S(va_list S);
+int print_pnt(va_list p);
 
 #endif
