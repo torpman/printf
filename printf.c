5527a60b91f58a70664148f2cc174152419107d7
torpman <torpman1112@gmail.com>
2022-04-19 20:11:57 -0700

    rot13 debugged

--- printf.c
+++ printf.c
@@ -22,7 +22,7 @@ static int (*check_for_specifiers(const char *format))(va_list)
 		{"x", print_ex},
 		{"X", print_HEX},
 		{"p", print_pnt},
-		{"R", print_R},
+		{"R", print_rot13},
 		{NULL, NULL}
 	};
 
