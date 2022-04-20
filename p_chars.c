f637b3e46c98acecf962bb5581812b3d55008e73
torpman <torpman1112@gmail.com>
2022-04-19 18:10:50 -0700

    S specifiers added

--- p_chars.c
+++ p_chars.c
@@ -14,6 +14,31 @@ int print_char(va_list c)
 }
 
 /**
+ * hex_print - prints a char's ascii value in uppercase hex
+ * @c: char to print
+ *
+ * Return: number of chars printed (always 2)
+ */
+static int hex_print(char c)
+{
+	int count;
+	char diff = 'A' - ':';
+	char d[2];
+
+	d[0] = c / 16;
+	d[1] = c % 16;
+	for (count = 0; count < 2; count++)
+	{
+		if (d[count] >= 10)
+			_print('0' + diff + d[count]);
+		else
+			_print('0' + d[count]);
+	}
+	return (count);
+}
+
+
+/**
  * print_str - prints a string
  * @s: string to print
  *
@@ -32,4 +57,34 @@ int print_str(va_list s)
 	}
 	return (count);
 }
+/**
+ * print_S - prints a string and nonprintable character ascii values
+ * @S: string to print
+ *
+ * Return: number of chars printed
+ */
+int print_S(va_list S)
+{
+	unsigned int i;
+	int count = 0;
+	char *str = va_arg(S, char *);
 
+	if (str == NULL)
+		str = "(null)";
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] < 32 || str[i] >= 127)
+		{
+			_print('\\');
+			_print('x');
+			count += 2;
+			count += hex_print(str[i]);
+		}
+		else
+		{
+			_print(str[i]);
+			count++;
+		}
+	}
+	return (count);
+}
